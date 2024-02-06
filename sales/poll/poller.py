20f9c7ffd3c1f8b3535ca62da945a9c41e7799c3
Gabe Wickert <gabewickert@gmail.com>
2024-02-06 10:20:08 -0800

    Fixed the Automobile Sales Poller.

--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -9,21 +9,26 @@ sys.path.append("")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sales_project.settings")
 django.setup()
 
-# Import models from sales_rest, here.
-# from sales_rest.models import Something
+from sales_rest.models import AutomobileVO
 
+def get_automobiles():
+    response = requests.get("http://project-beta-inventory-api-1:8000/api/automobiles/")
+    content = json.loads(response.content)
+    print(content)
+    for automobile in content["automobiles"]:
+        AutomobileVO.objects.update_or_create(
+            vin = automobile["vin"],
+            sold = automobile["sold"]
+        )
+        print(response.status_code)
 
 def poll():
     while True:
         print('Sales poller polling for data')
         try:
-            # Write your polling logic, here
-            # Do not copy entire file
-
-            pass
+            get_automobiles()
         except Exception as e:
-            print(e, file=sys.stderr)
-
+            print(e)
         time.sleep(60)
 
 
